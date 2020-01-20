fix hand coord loss calculation, added task combination experiment for pami paper

main_eval_mfnet.py
@@ -50,7 +50,7 @@ torch.set_printoptions(linewidth=1000000, threshold=1000000)
 
 def main():
     args = parse_args('mfnet', val=True)
-    tasks_per_dataset = parse_tasks_str(args.tasks, args.dataset)
+    tasks_per_dataset = parse_tasks_str(args)
     if args.eval_tasks is not None: # trained multi-dataset eval single-dataset
         eval_tasks_per_dataset = parse_tasks_str(args.eval_tasks, [args.eval_dataset])
         starting_cls_id, starting_g_id, starting_h_id = compare_tasks_per_dataset(tasks_per_dataset,

main_train_mfnet.py
@@ -13,6 +13,7 @@ import torch.backends.cudnn as cudnn
 import torchvision.transforms as transforms
 
 from src.models.mfnet_3d_mo import MFNET_3D_MO
+from src.models.mfnet_3d_mo_comb import MFNET_3D_MO_COMB
 from src.models.mfnet_3d_slowfast import MFNET_3D_SF
 from src.models.mfnet_3d_mo_mm import MFNET_3D_MO_MM
 from src.models.mfnet_3d_mo_dfb import MFNET_3D_DFB
@@ -26,7 +27,7 @@ from src.utils.dataset.dataset_loader import MultitaskDatasetLoader, MultitaskDa
 from src.utils.video_sampler import prepare_sampler
 from src.utils.dataset.dataset_loader_transforms import RandomScale, RandomCrop, RandomHorizontalFlip, RandomHLS_2, ToTensorVid,\
     Normalize, Resize, CenterCrop, PredefinedHorizontalFlip
-from src.utils.train_utils import train_mfnet_mo, test_mfnet_mo, test_mfnet_mo_map
+from src.utils.train_utils import train_mfnet_mo, test_mfnet_mo, test_mfnet_mo_map, train_mfnet_mo_comb, test_mfnet_mo_comb
 from src.utils.lr_utils import load_lr_scheduler
 from torch.optim.lr_scheduler import ReduceLROnPlateau
 from src.constants import *
@@ -34,7 +35,7 @@ from src.constants import *
 
 def main():
     args, model_name = parse_args('mfnet', val=False)
-    tasks_per_dataset = parse_tasks_str(args.tasks, args.dataset)
+    tasks_per_dataset = parse_tasks_str(args)
     objectives_text, objectives, task_sizes = parse_tasks_per_dataset(tasks_per_dataset)
     num_classes, num_coords, num_objects, num_obj_cat = task_sizes
     output_dir, log_file = init_folders(args.base_output_dir, model_name, args.resume, args.logging)
@@ -67,6 +68,8 @@ def main():
     elif args.tdn:
         mfnet_3d = MFNET_3D_TDN
         multioutput_loss = 3
+    elif args.map_tasks:
+        mfnet_3d = MFNET_3D_MO_COMB
     else:
         mfnet_3d = MFNET_3D_MO
         if args.only_flow:
@@ -126,7 +129,8 @@ def main():
                                           object_list_prefix=args.object_list_prefix[:],
                                           object_categories=args.object_cats[:],
                                           use_flow=args.flow, flow_transforms=train_transforms_flow,
-                                          only_flow=args.only_flow)
+                                          only_flow=args.only_flow,
+                                          map_to_epic=args.map_tasks)
     train_iterator = torch.utils.data.DataLoader(train_loader, batch_size=args.batch_size, shuffle=True,
                                                  num_workers=args.num_workers, pin_memory=True)
 
@@ -142,7 +146,8 @@ def main():
                                          object_list_prefix=args.object_list_prefix[:],
                                          object_categories=args.object_cats[:],
                                          use_flow=args.flow, flow_transforms=test_transforms_flow,
-                                         only_flow=args.only_flow)
+                                         only_flow=args.only_flow,
+                                         map_to_epic=args.map_tasks)
     test_iterator = torch.utils.data.DataLoader(test_loader, batch_size=args.batch_size, shuffle=False,
                                                 num_workers=args.num_workers, pin_memory=True)
 
@@ -155,15 +160,19 @@ def main():
 
     lr_scheduler = load_lr_scheduler(args.lr_type, args.lr_steps, optimizer, len(train_iterator))
 
-    train = train_mfnet_mo
-    test = test_mfnet_mo
+    if args.map_tasks:
+        train = train_mfnet_mo_comb
+        test = test_mfnet_mo_comb
+    else:
+        train = train_mfnet_mo
+        test = test_mfnet_mo
     num_cls_tasks = objectives[0]
     top1 = [0.0] * num_cls_tasks
     if args.eval_map_vl:
         mAP = [0.0] * num_cls_tasks
     for epoch in range(args.max_epochs):
-        train(model_ft, optimizer, train_iterator, tasks_per_dataset, epoch, log_file, args.gpus, lr_scheduler,
-              moo=args.moo, use_flow=args.flow, one_obj_layer=args.one_object_layer,
+        train(model_ft, optimizer, train_iterator, tasks_per_dataset, epoch, log_file, args.gpus,
+              lr_scheduler=lr_scheduler, moo=args.moo, use_flow=args.flow, one_obj_layer=args.one_object_layer,
               grad_acc_batches=args.grad_acc_batches, multioutput_loss=multioutput_loss, t_attn=args.t_attn)
         if (epoch+1) % args.eval_freq == 0:
             if args.eval_on_train:

src/models/mfnet_3d_mo_comb.py
@@ -17,13 +17,13 @@ from src.models.layers import CoordRegressionLayer, MultitaskClassifiers, Object
 from src.utils.initializer import xavier
 from torch.functional import F
 
-class MFNET_3D_MO(nn.Module):
+class MFNET_3D_MO_COMB(nn.Module):
     def __init__(self, num_classes, dropout=None, **kwargs):
-        super(MFNET_3D_MO, self).__init__()
+        super(MFNET_3D_MO_COMB, self).__init__()
         # support for arbitrary number of output layers, but it is the user's job to make sure they make sense
         # (e.g. actions->actions and not actions->verbs,nouns etc.)
-        self.num_classes = num_classes
-        self.num_coords = kwargs.get('num_coords', 0)
+        self.num_classes = num_classes[0:4] # to remove egtea VN tasks
+        self.num_coords = kwargs.get('num_coords', 0) - 2 # to remove one of the hand tasks
         self.num_objects = kwargs.get('num_objects', None)
         self.num_obj_cat = kwargs.get('num_obj_cat', None)
         self.one_object_layer = kwargs.get('one_object_layer', False)
@@ -99,7 +99,7 @@ class MFNET_3D_MO(nn.Module):
         if dropout:
             self.globalpool.add_module('dropout', nn.Dropout(p=dropout))
 
-        self.classifier_list = MultitaskClassifiers(c5_out, num_classes)
+        self.classifier_list = MultitaskClassifiers(c5_out, self.num_classes)
 
         if self.num_objects:
             for ii, no in enumerate(self.num_objects): # if there are more than one object presence layers, e.g. one per dataset
@@ -196,22 +196,22 @@ class MFNET_3D_MO(nn.Module):
 if __name__ == "__main__":
     import torch, time
     # ---------
-    kwargs = {'num_coords': 3, 'num_objects': None, 'num_obj_cat': None, 'one_object_layer': True,
+    kwargs = {'num_coords': 5, 'num_objects': None, 'num_obj_cat': None, 'one_object_layer': True,
               'ensemble_eval': False}
-    net = MFNET_3D_MO(num_classes=[106, 19, 53], dropout=0.5, **kwargs)
-    data = torch.randn(2, 3, 16, 224, 224, requires_grad=True)
+    net = MFNET_3D_MO_COMB(num_classes=[2513, 125, 352, 106, 19, 53], dropout=0.5, **kwargs)
+    data = torch.randn(1, 3, 16, 224, 224, requires_grad=True)
     net.cuda()
     data = data.cuda()
-    net.train()
+    net.eval()
     # loss = torch.tensor([10]).cuda()
     output = net(data)
-    # t0 = time.time()
-    # for i in range(10):
-    #     output = net(data)
-    # t1 = time.time()
-    # print('forward time:', t1 - t0)
+    t0 = time.time()
+    for i in range(10):
+        output = net(data)
+    t1 = time.time()
+    print('forward time:', t1 - t0)
     # h, htail = net.forward_shared_block(data)
     # coords, heatmaps, probabilities = net.forward_coord_layers(htail)
     # output = net.forward_cls_layers(h)
    # torch.save({'state_dict': net.state_dict()}, './tmp.pth')
-    print(len(output))
+   #  print(len(output))

src/utils/dataset/dataset_loader.py
@@ -169,13 +169,14 @@ class MultitaskDatasetLoader(torch.utils.data.Dataset):
     def __init__(self, sampler, split_files, dataset_names, tasks_per_dataset, batch_transform,
                  gaze_list_prefix, hand_list_prefix, object_list_prefix, object_categories,
                  validation=False, eval_gaze=False, vis_data=False, use_flow=False, flow_transforms=None,
-                 only_flow=False):
+                 only_flow=False, map_to_epic=False):
         self.sampler = sampler
         # 1-1 association between dataset name, split file and resp tasks
         assert len(dataset_names) == len(tasks_per_dataset)
         self.video_list = list()
         self.dataset_infos = dict()
         self.maximum_target_size = 0
+        map_gtea = False
         for i, (dataset_name, split_file, td) in enumerate(zip(dataset_names, split_files, tasks_per_dataset)):
             glp = gaze_list_prefix.pop(0) if 'G' in td else None
             hlp = hand_list_prefix.pop(0) if 'H' in td else None
@@ -193,6 +194,8 @@ class MultitaskDatasetLoader(torch.utils.data.Dataset):
                 cls_tasks = GTEA_CLS_TASKS
                 max_num_classes = LABELS_GTEA
                 sub_with_flow = 'clips_frames\\'
+                if map_to_epic:
+                    map_gtea = True
             elif dataset_name == 'somv1':
                 data_line = SOMETHINGV1DataLine
                 img_tmpl = '{:05d}.jpg'
@@ -217,7 +220,7 @@ class MultitaskDatasetLoader(torch.utils.data.Dataset):
                 sys.exit("Unknown dataset")
             video_list = read_samples_list(split_file, data_line)
             dat_info = DatasetInfo(i, dataset_name, data_line, img_tmpl, td, cls_tasks, max_num_classes, glp, hlp, olp,
-                                   ocp, video_list, sub_with_flow)
+                                   ocp, video_list, sub_with_flow, map_gtea)
             self.maximum_target_size = td['max_target_size'] if td['max_target_size'] > self.maximum_target_size else self.maximum_target_size
             self.dataset_infos[dataset_name] = dat_info
             self.video_list += video_list

src/utils/train_utils.py
@@ -12,11 +12,13 @@ Functions that are used in training the network.
 """
 import time
 import torch
+import copy
 
-from src.utils.calc_utils import AverageMeter, accuracy, init_training_metrics, init_test_metrics, init_map_metrics, update_per_dataset_metrics, charades_map
+from src.utils.calc_utils import AverageMeter, accuracy, init_training_metrics, init_test_metrics,\
+    update_per_dataset_metrics, charades_map
 from src.utils.learning_rates import CyclicLR
 from torch.optim.lr_scheduler import ReduceLROnPlateau
-from src.losses.mtl_losses import get_mtl_losses, multiobjective_gradient_optimization
+from src.losses.mtl_losses import get_mtl_losses, multiobjective_gradient_optimization, get_mtl_losses_comb
 from src.utils.eval_utils import *
 
 def mixup_data(x, y, alpha=1.0, use_cuda=True):
@@ -99,6 +101,130 @@ def val_outputs_per_dataset(network_output, targets, tasks_per_dataset, batch_id
             outputs_per_dataset[dataset_id].append(tmp_outputs)
 
     return outputs_per_dataset, targets_per_dataset
+
+def calc_losses_per_dataset_comb(network_outputs, targets, masks, tasks_per_dataset, comb_tasks_per_dataset,
+                                 batch_ids_per_dataset, is_training, base_gpu, metrics):
+
+    (outputs, coords, heatmaps, probabilities, objects, obj_cat) = network_outputs
+
+    # split task outputs
+    task_outputs = list()
+    task_outputs.append(outputs[0][batch_ids_per_dataset[0]])  # actions epic
+    task_outputs.append(outputs[1])  # verbs combined
+    task_outputs.append(outputs[2])  # nouns combined
+    task_outputs.append(outputs[3][batch_ids_per_dataset[1]])  # actions gtea
+
+    # split task targets
+    masks = masks.cuda(base_gpu)
+    targets = targets.cuda(base_gpu)
+    task_targets = list()
+    tmp_targets_0 = targets[batch_ids_per_dataset[0]].transpose(0, 1)
+    tmp_targets_1 = targets[batch_ids_per_dataset[1]].transpose(0, 1)
+    task_targets.append(tmp_targets_0[0])
+    task_targets.append(tmp_targets_0[1])
+    task_targets.append(tmp_targets_0[2])
+    task_targets.append(tmp_targets_1[0])
+    task_targets.append(tmp_targets_1[1])
+    task_targets.append(tmp_targets_1[2])
+
+    full_loss, partial_losses = get_mtl_losses_comb(task_outputs, coords, heatmaps, targets, masks, tasks_per_dataset,
+                                                    comb_tasks_per_dataset, batch_ids_per_dataset, base_gpu)
+
+    cls_losses, gaze_coord_losses, hand_coord_losses, object_losses, obj_cat_losses = partial_losses
+
+    # update metrics
+    full_losses, dataset_metrics = metrics
+
+    dataset_batch_size_0 = len(batch_ids_per_dataset[0])
+    dataset_batch_size_1 = len(batch_ids_per_dataset[1])
+    batch_size = dataset_batch_size_0 + dataset_batch_size_1
+
+    if dataset_batch_size_0 > 0:
+        t1, t5 = accuracy(task_outputs[0].detach().cpu(), task_targets[0].detach().cpu().long(), topk=(1, 5))
+        dataset_metrics[0]['top1_meters'][0].update(t1.item(), dataset_batch_size_0)
+        dataset_metrics[0]['top5_meters'][0].update(t5.item(), dataset_batch_size_0)
+        t1, t5 = accuracy(task_outputs[1][batch_ids_per_dataset[0]].detach().cpu(), task_targets[1].detach().cpu().long(), topk=(1, 5))
+        dataset_metrics[0]['top1_meters'][1].update(t1.item(), dataset_batch_size_0)
+        dataset_metrics[0]['top5_meters'][1].update(t5.item(), dataset_batch_size_0)
+        t1, t5 = accuracy(task_outputs[2][batch_ids_per_dataset[0]].detach().cpu(), task_targets[2].detach().cpu().long(), topk=(1, 5))
+        dataset_metrics[0]['top1_meters'][2].update(t1.item(), dataset_batch_size_0)
+        dataset_metrics[0]['top5_meters'][2].update(t5.item(), dataset_batch_size_0)
+    if dataset_batch_size_1 > 0:
+        t1, t5 = accuracy(task_outputs[3].detach().cpu(), task_targets[3].detach().cpu().long(), topk=(1, 5))
+        dataset_metrics[1]['top1_meters'][0].update(t1.item(), dataset_batch_size_1)
+        dataset_metrics[1]['top5_meters'][0].update(t5.item(), dataset_batch_size_1)
+        t1, t5 = accuracy(task_outputs[1][batch_ids_per_dataset[1]].detach().cpu(), task_targets[4].detach().cpu().long(), topk=(1, 5))
+        dataset_metrics[1]['top1_meters'][1].update(t1.item(), dataset_batch_size_1)
+        dataset_metrics[1]['top5_meters'][1].update(t5.item(), dataset_batch_size_1)
+        t1, t5 = accuracy(task_outputs[2][batch_ids_per_dataset[1]].detach().cpu(), task_targets[5].detach().cpu().long(), topk=(1, 5))
+        dataset_metrics[1]['top1_meters'][2].update(t1.item(), dataset_batch_size_1)
+        dataset_metrics[1]['top5_meters'][2].update(t5.item(), dataset_batch_size_1)
+
+    if is_training:
+        full_losses.update(full_loss.item(), batch_size)
+        if dataset_batch_size_0 > 0:
+            dataset_metrics[0]['cls_loss_meters'][0].update(cls_losses[0].item(), dataset_batch_size_0)
+            dataset_metrics[0]['cls_loss_meters'][1].update(cls_losses[1].item(), batch_size)
+            dataset_metrics[0]['cls_loss_meters'][2].update(cls_losses[2].item(), batch_size)
+            for i, hl in enumerate(hand_coord_losses):
+                dataset_metrics[0]['losses_hands'][i].update(hl.item(), batch_size)
+        if dataset_batch_size_1 > 0:
+            dataset_metrics[1]['cls_loss_meters'][0].update(cls_losses[3].item(), dataset_batch_size_1)
+            for i, gl in enumerate(gaze_coord_losses):
+                dataset_metrics[1]['losses_gaze'][i].update(gl.item(), dataset_batch_size_1)
+
+    return full_loss
+
+def make_to_print_comb(to_print, log_file, tasks_per_dataset, metrics, is_training):
+    full_losses, dataset_metrics = metrics
+    # make_to_print
+    if is_training:
+        to_print += '[F_Loss {:.4f}[avg:{:.4f}]\n\t'.format(full_losses.val, full_losses.avg)
+
+    # dataset_id = 0
+    num_cls_tasks = tasks_per_dataset[0]['num_cls_tasks']
+    top1_meters = dataset_metrics[0]['top1_meters']
+    top5_meters = dataset_metrics[0]['top5_meters']
+    if is_training:
+        num_h_tasks = tasks_per_dataset[0]['num_h_tasks']
+        losses_hands = dataset_metrics[0]['losses_hands']
+        cls_loss_meters = dataset_metrics[0]['cls_loss_meters']
+        for ind in range(num_cls_tasks):
+            to_print += 'T{}::loss {:.4f}[avg:{:.4f}], '.format(ind, cls_loss_meters[ind].val, cls_loss_meters[ind].avg)
+        for ind in range(num_h_tasks):
+            to_print += '[l_hcoo_{} {:.4f}[avg:{:.4f}], '.format(ind, losses_hands[ind].val, losses_hands[ind].avg)
+        for ind in range(num_cls_tasks):
+            if ind == 0:
+                to_print += '\n\t\t'
+            to_print += 'T{}::Top1 {:.3f}[avg:{:.3f}],Top5 {:.3f}[avg:{:.3f}],'.format(
+                ind, top1_meters[ind].val, top1_meters[ind].avg, top5_meters[ind].val, top5_meters[ind].avg)
+        to_print += "\n\t"
+    else: # not training
+        to_print += '\n\t'
+        to_print = append_to_print_cls_results(to_print, num_cls_tasks, top1_meters, top5_meters)
+
+    # dataset_id = 1
+    num_cls_tasks = tasks_per_dataset[1]['num_cls_tasks']
+    top1_meters = dataset_metrics[1]['top1_meters']
+    top5_meters = dataset_metrics[1]['top5_meters']
+    if is_training:
+        num_g_tasks = tasks_per_dataset[1]['num_g_tasks']
+        losses_gaze = dataset_metrics[1]['losses_gaze']
+        cls_loss_meters = dataset_metrics[1]['cls_loss_meters']
+        for ind in range(num_cls_tasks):
+            to_print += 'T{}::loss {:.4f}[avg:{:.4f}], '.format(ind, cls_loss_meters[ind].val, cls_loss_meters[ind].avg)
+        for ind in range(num_g_tasks):
+            to_print += '[l_gcoo_{} {:.4f}[avg:{:.4f}], '.format(ind, losses_gaze[ind].val, losses_gaze[ind].avg)
+        for ind in range(num_cls_tasks):
+            if ind == 0:
+                to_print += '\n\t\t'
+            to_print += 'T{}::Top1 {:.3f}[avg:{:.3f}],Top5 {:.3f}[avg:{:.3f}],'.format(
+                ind, top1_meters[ind].val, top1_meters[ind].avg, top5_meters[ind].val, top5_meters[ind].avg)
+    else: # not training
+        to_print += '\n\t'
+        to_print = append_to_print_cls_results(to_print, num_cls_tasks, top1_meters, top5_meters)
+
+    print_and_save(to_print, log_file)
 
 def calc_losses_per_dataset(network_output, targets, masks, tasks_per_dataset, batch_ids_per_dataset, one_obj_layer,
                             is_training, base_gpu, dataset_metrics, multioutput_loss=0, t_attn=False):
@@ -299,8 +425,52 @@ def make_final_test_print(tasks_per_dataset, dataset_metrics, dataset_type, log_
         print_and_save(final_print, log_file)
 
 
+def train_mfnet_mo_comb(model, optimizer, train_iterator, tasks_per_dataset, cur_epoch, log_file, gpus, **kwargs):
+    # reduce the tasks for egtea
+    comb_tasks_per_dataset = copy.deepcopy(tasks_per_dataset)
+    comb_tasks_per_dataset[1]['num_cls_tasks'] -= 2
+    comb_tasks_per_dataset[1]['num_h_tasks'] -= 1
+    batch_time, full_losses_metric, dataset_metrics = init_training_metrics(tasks_per_dataset, 0, False)
+
+    lr_scheduler = kwargs.get('lr_scheduler')
+
+    optimizer.zero_grad()
+    model.train()
+    is_training = True
+    if not isinstance(lr_scheduler, CyclicLR) and not isinstance(lr_scheduler, ReduceLROnPlateau):
+        lr_scheduler.step()
+
+    print_and_save('*********', log_file)
+    print_and_save('Beginning of epoch: {}'.format(cur_epoch), log_file)
+    t0 = time.time()
+
+    for batch_idx, data in enumerate(train_iterator):
+        if isinstance(lr_scheduler, CyclicLR):
+            lr_scheduler.step()
+
+        inputs, targets, masks, dataset_ids, batch_ids_per_dataset = init_inputs_batch(data, comb_tasks_per_dataset,
+                                                                                       False, gpus[0])
+        network_output = model(inputs)
+
+        full_loss = calc_losses_per_dataset_comb(network_output, targets, masks, tasks_per_dataset,
+                                                 comb_tasks_per_dataset, batch_ids_per_dataset, is_training, gpus[0],
+                                                 (full_losses_metric, dataset_metrics))
+
+        full_loss.backward()
+        optimizer.step()
+        batch_time.update(time.time() - t0)
+        t0 = time.time()
+        # print results
+        to_print = '[Epoch:{}, Batch {}/{} in {:.3f} s, LR {:.6f}]'.format(
+            cur_epoch, batch_idx, len(train_iterator), batch_time.val, lr_scheduler.get_lr()[0])
+        make_to_print_comb(to_print, log_file, tasks_per_dataset, (full_losses_metric, dataset_metrics), is_training)
+
+    print_and_save("Epoch train time: {}".format(batch_time.sum), log_file)
+
+
 def train_mfnet_mo(model, optimizer, train_iterator, tasks_per_dataset, cur_epoch, log_file, gpus, lr_scheduler=None,
-                   moo=False, use_flow=False, one_obj_layer=False, grad_acc_batches=None, multioutput_loss=0, t_attn=False):
+                   moo=False, use_flow=False, one_obj_layer=False, grad_acc_batches=None, multioutput_loss=0,
+                   t_attn=False):
     batch_time, full_losses_metric, dataset_metrics = init_training_metrics(tasks_per_dataset, multioutput_loss, t_attn)
 
     optimizer.zero_grad()
@@ -364,6 +534,37 @@ def train_mfnet_mo(model, optimizer, train_iterator, tasks_per_dataset, cur_epoc
                           dataset_ids, full_loss, multioutput_loss, t_attn)
 
     print_and_save("Epoch train time: {}".format(batch_time.sum), log_file)
+
+def test_mfnet_mo_comb(model, test_iterator, tasks_per_dataset, cur_epoch, dataset_type, log_file, gpus, **kwargs):
+    is_training = False
+    # reduce the tasks for egtea
+    comb_tasks_per_dataset = copy.deepcopy(tasks_per_dataset)
+    comb_tasks_per_dataset[1]['num_cls_tasks'] -= 2
+    comb_tasks_per_dataset[1]['num_h_tasks'] -= 1
+    dataset_metrics = init_test_metrics(tasks_per_dataset)
+
+    with torch.no_grad():
+        model.eval()
+        print_and_save('Evaluating after epoch: {} on {} set'.format(cur_epoch, dataset_type), log_file)
+        for batch_idx, data in enumerate(test_iterator):
+            inputs, targets, masks, dataset_ids, batch_ids_per_dataset = init_inputs_batch(data, comb_tasks_per_dataset,
+                                                                                           False, gpus[0])
+
+            network_output = model(inputs)
+            _ = calc_losses_per_dataset_comb(network_output, targets, masks, tasks_per_dataset, comb_tasks_per_dataset,
+                                             batch_ids_per_dataset, is_training, gpus[0], (None, dataset_metrics))
+            # print results
+            to_print = '[Epoch:{}, Batch {}/{}]'.format(cur_epoch, batch_idx, len(test_iterator))
+            make_to_print_comb(to_print, log_file, tasks_per_dataset, (None, dataset_metrics), is_training)
+        make_final_test_print(tasks_per_dataset, dataset_metrics, dataset_type, log_file)
+
+        task_top1s = list()
+        for dataset_id in range(len(tasks_per_dataset)):
+            top1_meters = dataset_metrics[dataset_id]['top1_meters']
+            for tasktop1 in top1_meters:
+                task_top1s.append(tasktop1.avg)
+
+    return task_top1s
 
 def test_mfnet_mo(model, test_iterator, tasks_per_dataset, cur_epoch, dataset_type, log_file, gpus, use_flow=False,
                   one_obj_layer=False, multioutput_loss=0, t_attn=False):